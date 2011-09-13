37f4f9bd5616317b31eff0da6b35fc87ef339091
Eloy Duran <eloy.de.enige@gmail.com>
2011-09-13 22:34:59 +0200

    Make pod install take an optional podspec file to install the dependencies of instead of looking for ./Podfile

--- lib/cocoa_pods/command/install.rb
+++ lib/cocoa_pods/command/install.rb
@@ -1,13 +1,29 @@
 module Pod
   class Command
     class Install < Command
-      def run
-        if config.project_podfile.exist?
-          spec = Specification.from_podfile(config.project_podfile)
-          Installer.new(spec, config.project_pods_root).install!
-        else
-          $stderr.puts "No Podfile found in current working directory."
+      def initialize(*argv)
+        if podspec = argv.shift
+          @podspec = Pathname.new(podspec)
         end
+        super
+      end
+
+      def run
+        spec = nil
+        if @podspec
+          if @podspec.exist?
+            spec = Specification.from_podspec(@podspec)
+          else
+            raise "The specified podspec `#{@podspec}' doesn't exist."
+          end
+        else
+          if config.project_podfile.exist?
+            spec = Specification.from_podfile(config.project_podfile)
+          else
+            raise "No Podfile found in current working directory."
+          end
+        end
+        Installer.new(spec, config.project_pods_root).install!
       end
     end
   end
