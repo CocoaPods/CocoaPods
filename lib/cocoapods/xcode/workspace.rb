41391400ccb8d6e037420a51e826be6a8022a9c4
Nolan Waite <nolan@nolanw.ca>
2011-10-22 07:58:14 -0600

    Load/save xcworkspace

--- lib/cocoapods/xcode/workspace.rb
+++ lib/cocoapods/xcode/workspace.rb
@@ -1,4 +1,5 @@
 framework 'Foundation'
+require 'fileutils'
 
 module Pod
   module Xcode
@@ -6,6 +7,20 @@ module Pod
       def initialize(*projpaths)
         @projpaths = projpaths
       end
+      
+      def self.from_xcworkspace(path)
+        from_s(File.read(File.join(path, 'contents.xcworkspacedata')))
+      end
+      
+      def self.from_s(xml)
+        doc = NSXMLDocument.alloc.initWithXMLString(xml, options:0, error:nil)
+        projpaths = doc.nodesForXPath("/Workspace/FileRef", error:nil).map do |node|
+          node.attributeWithName("location").stringValue.sub(/^group:/, '')
+        end
+        new(projpaths)
+      end
+      
+      attr_reader :projpaths
       
       def <<(projpath)
         @projpaths << projpath
@@ -20,6 +35,13 @@ module Pod
           doc.rootElement.addChild(el)
         end
         NSString.alloc.initWithData(doc.XMLData, encoding:NSUTF8StringEncoding)
+      end
+      
+      def save_as(path)
+        FileUtils.mkdir_p(path)
+        File.open(File.join(path, 'contents.xcworkspacedata'), 'w') do |out|
+          out << to_s
+        end
       end
     end
   end
--- spec/unit/xcode/workspace_spec.rb
+++ spec/unit/xcode/workspace_spec.rb
@@ -3,14 +3,22 @@ require File.expand_path('../../../spec_helper', __FILE__)
 describe "Pod::Xcode::Workspace" do
   before do
     @workspace = Pod::Xcode::Workspace.new('Pods/Pods.xcodeproj', 'App.xcodeproj')
+  end
+  
+  it "accepts new projects" do
+    @workspace << 'Framework.xcodeproj'
+    @workspace.projpaths.should.include 'Framework.xcodeproj'
+  end
+    
+  before do
     @doc = NSXMLDocument.alloc.initWithXMLString(@workspace.to_s, options:0, error:nil)
   end
   
-  it "is the right workspace version" do
+  it "is the right xml workspace version" do
     @doc.rootElement.attributeForName("version").stringValue.should == "1.0"
   end
   
-  it "contains the projects" do
+  it "refers to the projects in xml" do
     @doc.nodesForXPath("/Workspace/FileRef", error:nil).map do |node|
       node.attributeForName("location").stringValue.sub(/^group:/, '')
     end.sort.should == ['App.xcodeproj', 'Pods/Pods.xcodeproj']
