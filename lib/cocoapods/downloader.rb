e08be2fec8b33e7222bd91a96e8a54cceefbb91c
Eloy Duran <eloy.de.enige@gmail.com>
2011-11-16 01:02:33 +0100

    Stash hacking.

--- lib/cocoapods/downloader.rb
+++ lib/cocoapods/downloader.rb
@@ -1,9 +1,13 @@
 module Pod
   class Downloader
+    extend Executable
+
     def self.for_source(pod_root, source)
       options = source.dup
       if url = options.delete(:git)
         Git.new(pod_root, url, options)
+      elsif url = options.delete(:hg)
+        Mercurial.new(pod_root, url, options)
       else
         raise "Unsupported download strategy `#{source.inspect}'."
       end
@@ -15,8 +19,13 @@ module Pod
       @pod_root, @url, @options = pod_root, url, options
     end
 
+    def clean(clean_paths = [])
+      clean_paths.each do |path|
+        path.rmtree
+      end if clean_paths
+    end
+
     class Git < Downloader
-      extend Executable
       executable :git
 
       def download
@@ -53,10 +62,36 @@ module Pod
       end
 
       def clean(clean_paths = [])
+        super
         (@pod_root + '.git').rmtree
-        clean_paths.each do |path|
-          path.rmtree
-        end if clean_paths
+      end
+    end
+
+    class Mercurial < Downloader
+      extend Executable
+      executable :hg
+
+      def download
+        @pod_root.dirname.mkpath
+        if @options[:revision]
+          download_revision
+        else
+          download_head
+        end
+      end
+
+      def download_head
+        hg "clone '#{@url}' '#{@pod_root}'"
+      end
+
+      def download_revision
+        hg "clone '#{@url}' --rev '#{@options[:revision]}' '#{@pod_root}'"
+      end
+
+      def clean(clean_paths = [])
+        super
+        #(@pod_root + '.git').rmtree
+        puts "TODO clean mercurial!"
       end
     end
   end
