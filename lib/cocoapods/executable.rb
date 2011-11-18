75d35a23239e97c5d760d1301d72b6511f5b6bb7
Eloy Duran <eloy.de.enige@gmail.com>
2011-11-18 23:09:56 +0100

    Improve error reporting if an executable can't be found.

--- lib/cocoapods/executable.rb
+++ lib/cocoapods/executable.rb
@@ -1,12 +1,16 @@
 module Pod
   module Executable
     def executable(name)
+      bin = `which #{name}`.strip
       define_method(name) do |command|
+        if bin.empty?
+          raise Informative, "Unable to locate the executable `#{name}'"
+        end
         if Config.instance.verbose?
-          puts "#{name} #{command}"
-          `#{name} #{command} 1>&2`
+          puts "#{bin} #{command}"
+          `#{bin} #{command} 1>&2`
         else
-          `#{name} #{command} 2> /dev/null`
+          `#{bin} #{command} 2> /dev/null`
         end
       end
       private name
