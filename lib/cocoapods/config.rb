55955d6ac5f4c5d910d0efc8338d1bf198cec3fa
Ben Scheirman <ben@scheirman.com>
2012-02-24 09:50:25 -0600

    Store the source_root in config (if it is passed in).

--- lib/cocoapods/config.rb
+++ lib/cocoapods/config.rb
@@ -10,7 +10,7 @@ module Pod
       @instance = instance
     end
 
-    attr_accessor :repos_dir, :project_root, :project_pods_root, :rootspec, :clean, :verbose, :silent
+    attr_accessor :repos_dir, :project_root, :source_root, :project_pods_root, :rootspec, :clean, :verbose, :silent
     alias_method :clean?,   :clean
     alias_method :verbose?, :verbose
     alias_method :silent?,  :silent
@@ -24,6 +24,10 @@ module Pod
 
     def project_root
       @project_root ||= Pathname.pwd
+    end
+
+    def source_root
+      @source_root ||= project_root
     end
 
     def project_pods_root
