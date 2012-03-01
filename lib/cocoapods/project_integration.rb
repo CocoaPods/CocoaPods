794a427f69c41d4d7d7deb9e2aa99e4b2c28df97
Luke Redpath <luke@lukeredpath.co.uk>
2012-03-01 00:16:09 +0000

    Its not much of a refactoring, but things are a bit clearer now.

--- lib/cocoapods/project_integration.rb
+++ lib/cocoapods/project_integration.rb
@@ -17,44 +17,72 @@ module Pod
     #    let the user specify it with the proper case.)
     # 3. Let the user specify the app target name as an extra argument, but this
     #    seems to be a less good version of the variation on #2.
-    
-    def self.integrate_with_project(projpath)
-      # TODO use more of Pathname’s API here
-      root = File.dirname(projpath)
-      xcworkspace = File.join(root, File.basename(projpath, '.xcodeproj') + '.xcworkspace')
-      workspace = Xcodeproj::Workspace.new_from_xcworkspace(xcworkspace)
-      pods_projpath = File.join(config.project_pods_root, 'Pods.xcodeproj')
-      root = Pathname.new(root).expand_path
-      [projpath, pods_projpath].each do |path|
-        path = Pathname.new(path).expand_path.relative_path_from(root).to_s
-        workspace << path unless workspace.include? path
-      end
-      workspace.save_as(xcworkspace)
 
-      app_project = Xcodeproj::Project.new(projpath)
-      return if app_project.files.find { |file| file.path =~ /libPods\.a$/ }
+    class << self
+      def integrate_with_project(projpath)
+        root = File.dirname(projpath)
+        name = File.basename(projpath, '.xcodeproj')
+        
+        xcworkspace = create_workspace(root, name, projpath)
+        app_project = Xcodeproj::Project.new(projpath)
 
-      configfile = app_project.files.new('path' => 'Pods/Pods.xcconfig')
-      app_project.targets.each do |target|
-        target.buildConfigurations.each do |config|
-          config.baseConfiguration = configfile
+        return if project_already_integrated?(app_project)
+
+        xcconfig = app_project.files.new('path' => 'Pods/Pods.xcconfig')
+        base_project_configurations_on_xcconfig(app_project, xcconfig)
+      
+        libfile = app_project.files.new_static_library('Pods')
+        libfile.group = app_project.group("Frameworks")
+        
+        add_pods_library_to_each_target_in_project(app_project, libfile)
+      
+        copy_resources = app_project.add_shell_script_build_phase(
+          'Copy Pods Resources', %{"${SRCROOT}/Pods/Pods-resources.sh"\n})
+          
+        add_copy_resources_script_phase_to_each_target_in_project(app_project, copy_resources)
+      
+        app_project.save_as(projpath)
+
+        unless config.silent?
+          # TODO this really shouldn't be here
+          puts "[!] From now on use `#{File.basename(xcworkspace)}' instead of `#{File.basename(projpath)}'."
         end
       end
       
-      libfile = app_project.files.new_static_library('Pods')
-      libfile.group = app_project.main_group.groups.find { |g| g.name == 'Frameworks' }
-      app_project.objects.select_by_class(Xcodeproj::Project::Object::PBXFrameworksBuildPhase).each do |build_phase|
-        build_phase.files << libfile.buildFiles.new
+      def create_workspace(in_directory, name, project_path)
+        workspace_path = File.join(in_directory, name + '.xcworkspace')
+        workspace = Xcodeproj::Workspace.new_from_xcworkspace(workspace_path)
+        pods_project_path = File.join(config.project_pods_root, 'Pods.xcodeproj')
+        root = Pathname.new(in_directory).expand_path
+        [project_path, pods_project_path].each do |path|
+          path = Pathname.new(path).expand_path.relative_path_from(root).to_s
+          workspace << path unless workspace.include?(path)
+        end
+        workspace.save_as(workspace_path)
       end
       
-      copy_resources = app_project.add_shell_script_build_phase('Copy Pods Resources',
-%{"${SRCROOT}/Pods/Pods-resources.sh"\n})
-      app_project.targets.each { |target| target.buildPhases << copy_resources }
+      def project_already_integrated?(project)
+        project.files.find { |file| file.path =~ /libPods\.a$/ }
+      end
       
-      app_project.save_as(projpath)
-
-      unless config.silent?
-        puts "[!] From now on use `#{File.basename(xcworkspace)}' instead of `#{File.basename(projpath)}'."
+      def base_project_configurations_on_xcconfig(project, xcconfig_file)
+        project.targets.each do |target|
+          target.buildConfigurations.each do |config|
+            config.baseConfiguration = xcconfig_file
+          end
+        end
+      end
+      
+      def add_pods_library_to_each_target_in_project(project, pods_library)
+        project.targets.each do |target|
+          target.frameworks_build_phases.each do |build_phase|
+            build_phase.files << pods_library.buildFiles.new
+          end
+        end
+      end
+      
+      def add_copy_resources_script_phase_to_each_target_in_project(project, copy_resources_script_phase)
+        project.targets.each { |target| target.buildPhases << copy_resources_script_phase }
       end
     end
   end
