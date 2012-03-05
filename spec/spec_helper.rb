040f22f76fa2c591d183bc68267fcb7d1742f3db
Luke Redpath <luke@lukeredpath.co.uk>
2012-03-05 01:37:25 +0000

    Allow HTTP connections outside of VCR blocks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -56,4 +56,5 @@ require 'webmock'
 VCR.configure do |c|
   c.cassette_library_dir = (ROOT + 'spec/fixtures/vcr').to_s
   c.hook_into :webmock # or :fakeweb
+  c.allow_http_connections_when_no_cassette = true
 end
