d68d308bd1bf29de6a27d5990973874844f46567
Fabio Pelosin <fabiopelosin@gmail.com>
2012-03-24 18:57:53 +0100

    [list-new] Extratect pod display logic from pod search.

--- lib/cocoapods/command/search.rb
+++ lib/cocoapods/command/search.rb
@@ -1,6 +1,9 @@
 module Pod
   class Command
     class Search < Command
+
+      include DisplayPods
+
       def self.banner
         %{Search pods:
 
@@ -26,45 +29,8 @@ module Pod
       end
 
       def run
-        Source.search_by_name(@query.strip, @full_text_search).each do |set|
-          puts "\e[32m--> #{set.name} (#{set.versions.reverse.join(", ")})\e[0m"
-          puts_wrapped_text(set.specification.summary)
-
-          spec = set.specification.part_of_other_pod? ? set.specification.part_of_specification : set.specification
-
-          source = spec.source.reject {|k,_| k == :commit || k == :tag }.values.first
-          puts_detail('Homepage', spec.homepage)
-          puts_detail('Source', source)
-          puts_github_info(source) if @stats
-
-          puts
-        end
-      end
-
-      # adapted from http://blog.macromates.com/2006/wrapping-text-with-regular-expressions/
-      def puts_wrapped_text(txt, col = 80, indentation = 4)
-        indent = ' ' * indentation
-        puts txt.strip.gsub(/(.{1,#{col}})( +|$)\n?|(.{#{col}})/, indent + "\\1\\3\n")
-      end
-
-      def puts_detail(title,string)
-        return if !string
-        # 8 is the length of homepage which might be displayed alone
-        number_of_spaces = ((8 - title.length) > 0) ? (8 - title.length) : 0
-        spaces = ' ' * number_of_spaces
-        puts "    - #{title}: #{spaces + string}"
-      end
-
-      def puts_github_info(url)
-        original_url, username, reponame = *(url.match(/[:\/]([\w\-]+)\/([\w\-]+)\.git/).to_a)
-
-        if original_url
-          repo_info = `curl -s -m 2 http://github.com/api/v2/json/repos/show/#{username}/#{reponame}`
-          watchers = repo_info.match(/\"watchers\"\W*:\W*([0-9]+)/).to_a[1]
-          forks = repo_info.match(/\"forks\"\W*:\W*([0-9]+)/).to_a[1]
-          puts_detail('Watchers', watchers)
-          puts_detail('Forks', forks)
-        end
+        sets = Source.search_by_name(@query.strip, @full_text_search)
+        display_pod_list(sets, @stats)
       end
     end
   end
