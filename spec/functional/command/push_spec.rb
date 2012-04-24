3b074a3d6cb8f3a81080f22aac3ffbb299807a62
Fabio Pelosin <fabiopelosin@gmail.com>
2012-04-24 18:35:44 +0200

    [Specs] more fixes - Ooooops edition

--- spec/functional/command/push_spec.rb
+++ spec/functional/command/push_spec.rb
@@ -44,7 +44,7 @@ describe Pod::Command::Push do
 
     # prepare the spec
     spec_fix = (fixture('spec-repos') + 'master/JSONKit/1.4/JSONKit.podspec').read
-    spec_add = spec_fix.gsub(/https:\/\/github\.com\/johnezang\/JSONKit\.git/, fixture('integration/JSONKit'))
+    spec_add = spec_fix.gsub(/https:\/\/github\.com\/johnezang\/JSONKit\.git/, fixture('integration/JSONKit').to_s)
     spec_add.gsub!(/'JSONKit'/, "'PushTest'")
     File.open(temporary_directory + 'JSONKit.podspec', 'w') {|f| f.write(spec_fix) }
     File.open(temporary_directory + 'PushTest.podspec', 'w') {|f| f.write(spec_add) }
