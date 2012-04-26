fb637f2f646e89fb3055ed24f64f52d1c460d1d5
Will Pragnell <wpragnell@gmail.com>
2012-04-26 23:34:29 +0100

    Changed all single quotes to double quotes (OCD?)

--- spec/unit/generator/acknowledgements_spec.rb
+++ spec/unit/generator/acknowledgements_spec.rb
@@ -1,40 +1,40 @@
-require File.expand_path('../../../spec_helper', __FILE__)
+require File.expand_path("../../../spec_helper", __FILE__)
 
 describe Pod::Generator::Acknowledgements do
   before do
     @podfile = Pod::Podfile.new do
       platform :ios
-      xcodeproj 'dummy'
+      xcodeproj "dummy"
     end
     @target_definition = @podfile.target_definitions[:default]
 
     @sandbox = temporary_sandbox
-    @pods = [Pod::LocalPod.new(fixture_spec('banana-lib/BananaLib.podspec'), @sandbox, Pod::Platform.ios)]
-    copy_fixture_to_pod('banana-lib', @pods[0])
+    @pods = [Pod::LocalPod.new(fixture_spec("banana-lib/BananaLib.podspec"), @sandbox, Pod::Platform.ios)]
+    copy_fixture_to_pod("banana-lib", @pods[0])
     @acknowledgements = Pod::Generator::Acknowledgements.new(@target_definition, @pods)
   end
 
-  it 'returns the corrent number of licenses (including headera and footnote)' do
+  it "returns the corrent number of licenses (including headera and footnote)" do
     @acknowledgements.licenses.count.should == 3
   end
 
-  it 'returns a correctly formed license hash for each pod' do
+  it "returns a correctly formed license hash for each pod" do
     @acknowledgements.hash_for_pod(@pods[0]).should == {
-      :Type => 'PSGroupSpecifier',
-      :Title => 'BananaLib',
-      :FooterText => 'Permission is hereby granted ...'
+      :Type => "PSGroupSpecifier",
+      :Title => "BananaLib",
+      :FooterText => "Permission is hereby granted ..."
     }
   end
 
-  it 'returns a plist containg the licenses' do
+  it "returns a plist containg the licenses" do
     @acknowledgements.plist.should == {
-      :Title => 'Acknowledgements',
-      :StringsTable => 'Acknowledgements',
+      :Title => "Acknowledgements",
+      :StringsTable => "Acknowledgements",
       :PreferenceSpecifiers => @acknowledgements.licenses
     }
   end
 
-  it 'writes a plist to disk' do
+  it "writes a plist to disk" do
     @acknowledgements.save_as(@sandbox.root + 'test.plist').should.be.true
   end
 end
