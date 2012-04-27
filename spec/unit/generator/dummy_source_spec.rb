967b8aba770ce06b57dc88b549817e336fcdfd70
Francis Chong <siu.ying@gmail.com>
2012-04-27 10:13:16 +0800

    implement unit test for dummy source

--- spec/unit/generator/dummy_source_spec.rb
+++ spec/unit/generator/dummy_source_spec.rb
@@ -1,5 +1,29 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
 describe Pod::Generator::DummySource do
-  it "generates a dummy sourcefile with the appropriate class"
+  extend SpecHelper::TemporaryDirectory
+
+  before do
+    setup_temporary_directory
+  end
+  
+  after do
+    teardown_temporary_directory
+  end
+  
+  def do_install!
+    @installer.install!(@pods, @sandbox)
+  end
+
+  it "generates a dummy sourcefile with the appropriate class" do
+    generator = Pod::Generator::DummySource.new("Pods")
+    file = temporary_directory + 'PodsDummy.m'
+    generator.save_as(file)
+    file.read.should == <<-EOS
+@interface PodsDummy : NSObject
+@end
+@implementation PodsDummy
+@end
+EOS
+  end  
 end
