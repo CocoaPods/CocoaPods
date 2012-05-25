7ba101a5c0b5407af79dfd095a43557611ad32aa
Fabio Pelosin <fabiopelosin@gmail.com>
2012-05-25 16:16:24 +0200

    [Acknowledgements] Fine tune to markdown output.
    Discussion at #250
    Added white space to the titles and made header title h1
    instead of an h2.

--- lib/cocoapods/generator/acknowledgements/markdown.rb
+++ lib/cocoapods/generator/acknowledgements/markdown.rb
@@ -12,26 +12,26 @@ module Pod
         file.close
       end
 
-      def title_from_string(string)
+      def title_from_string(string, level)
         if !string.empty?
-          "#{string}\n" + '-' * string.length + "\n"
+          "#" * level << " #{string}"
         end
       end
 
       def string_for_pod(pod)
         if (license_text = pod.license_text)
-          title_from_string(pod.name) + license_text + "\n"
+          "\n" << title_from_string(pod.name, 2) << "\n\n" << license_text << "\n"
         end
       end
 
       def licenses
-        licenses_string = "#{title_from_string(header_title)}#{header_text}\n"
+        licenses_string = "#{title_from_string(header_title, 1)}\n#{header_text}\n"
         @pods.each do |pod|
           if (license = string_for_pod(pod))
             licenses_string += license
           end
         end
-        licenses_string += "#{title_from_string(footnote_title)}#{footnote_text}\n"
+        licenses_string += "#{title_from_string(footnote_title, 2)}#{footnote_text}\n"
       end
     end
   end
