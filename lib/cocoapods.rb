79bd3e24126ed93f10da969bae52c623d95fc4da
Eloy Duran <eloy.de.enige@gmail.com>
2012-07-14 18:25:48 +0200

    Release 0.9.0

--- lib/cocoapods.rb
+++ lib/cocoapods.rb
@@ -13,7 +13,7 @@ unless Gem::Version::Requirement.new('>= 1.4.0').satisfied_by?(Gem::Version.new(
 end
 
 module Pod
-  VERSION = '0.8.0'
+  VERSION = '0.9.0'
 
   class PlainInformative < StandardError
   end
