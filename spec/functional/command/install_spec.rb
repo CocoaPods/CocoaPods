451a526d8ab9f3ce2b312c6a6a6defc7bd53504f
Eloy Durán <eloy.de.enige@gmail.com>
2012-10-26 14:59:23 +0200

    Ensure the install command test works by having a clean env.

--- spec/functional/command/install_spec.rb
+++ spec/functional/command/install_spec.rb
@@ -2,6 +2,8 @@ require File.expand_path('../../../spec_helper', __FILE__)
 
 module Pod
   describe Command::Install do
+    extend SpecHelper::TemporaryDirectory
+
     it "tells the user that no Podfile or podspec was found in the current working dir" do
       exception = lambda { run_command('install', '--no-update') }.should.raise Informative
       exception.message.should.include "No `Podfile' found in the current working directory."
