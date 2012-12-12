e6e4b6ab28773e1f55638631e59c1cc442eb7166
Fabio Pelosin <fabiopelosin@gmail.com>
2012-12-13 00:32:51 +0100

    Remove the Rubygems version check as now it is handled by cocoapods-core.

--- lib/cocoapods.rb
+++ lib/cocoapods.rb
@@ -1,19 +1,12 @@
 require 'rubygems'
 
-# Better to fail early and clear then during installation of pods.
-#
-# RubyGems 1.3.6 (which ships with OS X >= 10.7) up to 1.4.0 have a couple of
-# bugs related to comparing prerelease versions.
-#
-# E.g. https://github.com/CocoaPods/CocoaPods/issues/398
-unless Gem::Version::Requirement.new('>= 1.4.0').satisfied_by?(Gem::Version.new(Gem::VERSION))
-  STDERR.puts "\e[1;31m" + "Your RubyGems version (#{Gem::VERSION}) is too old, please update with: `gem update --system`" + "\e[0m"
-  exit 1
-end
-
 module Pod
-  class PlainInformative < StandardError
-  end
+  require 'cocoapods-core'
+  require 'cocoapods/downloader'
+  require 'cocoapods/file_list'
+  require 'cocoapods/config'
+
+  class PlainInformative < StandardError; end
 
   class Informative < PlainInformative
     def message
@@ -49,11 +42,6 @@ module Pod
     autoload :XCConfig,             'cocoapods/generator/xcconfig'
   end
 
-  require 'cocoapods-core'
-  require 'cocoapods/downloader'
-
-  require 'cocoapods/file_list'
-  require 'cocoapods/config'
 end
 
 if ENV['COCOA_PODS_ENV'] == 'development'
