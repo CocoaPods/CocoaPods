71f4eb1745f5842a87323e2acb14fb3160a76a56
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2013-02-20 12:37:26 -0800

    Fix a couple of minor typos in the unit tests

--- spec/unit/version_spec.rb
+++ spec/unit/version_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path('../../spec_helper', __FILE__)
 
 module Pod
   describe Version do
-    it "returns wether or not it's a `bleeding edge' version" do
+    it "returns whether or not it's a `bleeding edge' version" do
       version = Version.new('1.2.3')
       version.should.not.be.head
       version.head = true
