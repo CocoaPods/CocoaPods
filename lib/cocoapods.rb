0e40a6c175aef40dc7e5b2b82cc9a311ad97ad72
Eloy Durán <eloy.de.enige@gmail.com>
2013-02-20 22:46:00 +0100

    Release 0.16.3

--- lib/cocoapods.rb
+++ lib/cocoapods.rb
@@ -12,7 +12,7 @@ unless Gem::Version::Requirement.new('>= 1.4.0').satisfied_by?(Gem::Version.new(
 end
 
 module Pod
-  VERSION = '0.16.2'
+  VERSION = '0.16.3'
 
   class PlainInformative < StandardError
   end
