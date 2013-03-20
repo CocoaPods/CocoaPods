199d829031f7f08ded1067e3c5df2f2cd033fc7d
Fabio Pelosin <fabiopelosin@gmail.com>
2013-03-20 17:09:46 +0100

    Release 0.17.0.rc6

--- lib/cocoapods/gem_version.rb
+++ lib/cocoapods/gem_version.rb
@@ -2,6 +2,6 @@ module Pod
 
   # The version of the cocoapods command line tool.
   #
-  VERSION = '0.17.0.rc5' unless defined? Pod::VERSION
+  VERSION = '0.17.0.rc6' unless defined? Pod::VERSION
 end
 
