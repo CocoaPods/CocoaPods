caa30a8cf5e3221b50f8f9103cceaba9a502b83d
Fabio Pelosin <fabiopelosin@gmail.com>
2013-05-17 14:55:27 +0200

    [CopyResourcesScript] Restore compatiblity with Ruby 1.8.7

--- lib/cocoapods/generator/copy_resources_script.rb
+++ lib/cocoapods/generator/copy_resources_script.rb
@@ -2,8 +2,6 @@ module Pod
   module Generator
     class CopyResourcesScript
 
-      require 'fileutils'
-
       # @return [Array<#to_s>] A list of files relative to the project pods
       #         root.
       #
@@ -33,7 +31,7 @@ module Pod
         pathname.open('w') do |file|
           file.puts(script)
         end
-        FileUtils.chmod('+x', pathname.to_s)
+        File.chmod(755, pathname.to_s)
       end
 
       private
