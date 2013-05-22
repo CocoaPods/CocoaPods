3ccb10ccae575b422ff90455cba59809d9a73715
Fabio Pelosin <fabiopelosin@gmail.com>
2013-05-22 15:05:56 +0200

    [Fixtures] Update for changes to CopyResourcesScript

--- spec/integration/install_subspecs/after/Pods/Pods-OS X App-resources.sh
+++ spec/integration/install_subspecs/after/Pods/Pods-OS X App-resources.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+RESOURCES_TO_COPY=${PODS_ROOT}/resources-to-copy.txt
+touch "$RESOURCES_TO_COPY"
+
 install_resource()
 {
   case $1 in
@@ -20,8 +23,11 @@ install_resource()
       xcrun momc "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename $1 .xcdatamodeld`.momd"
       ;;
     *)
-      echo "rsync -av --exclude '*/.svn/*' ${PODS_ROOT}/$1 ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
-      rsync -av --exclude '*/.svn/*' "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+      echo "${PODS_ROOT}/$1"
+      echo "${PODS_ROOT}/$1" >> "$RESOURCES_TO_COPY"
       ;;
   esac
 }
+
+rsync -avr --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+rm "$RESOURCES_TO_COPY"
