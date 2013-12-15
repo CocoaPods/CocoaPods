5699bf84eaf156187eb1912988467f1ce63b9ffa
Jesper84 <jesper@metallicafan.dk>
2013-12-15 09:41:32 +0100

    [Command::Lib::Create] Show link to Making a CocoaPod guide
    Closes #1644
    Closes #1683

--- spec/functional/command/lib_spec.rb
+++ spec/functional/command/lib_spec.rb
@@ -1,7 +1,5 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-require 'xcodeproj'
-
 module Pod
   describe Command::Lib::Create do
     it "complains if wrong parameters" do
@@ -30,5 +28,19 @@ module Pod
         end
     end
   end
-end
 
+  describe Command::Lib do
+
+    it "should create a new dir for the newly created pod" do
+      run_command('lib', 'create', 'TestPod')
+      Dir.chdir(temporary_directory) do
+          Pathname.new(temporary_directory + 'TestPod').exist?.should == true
+      end
+    end
+
+    it "should show link to new pod guide after creation" do
+      output = run_command('lib', 'create', 'TestPod')
+      output.should.include? 'http://guides.cocoapods.org/making/making-a-cocoapod'
+    end
+  end
+end
