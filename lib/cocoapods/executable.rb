93fd9a6b12f4f6854cbf514c1b6b1f02ffb44270
Stephen Tramer <stephen.tramer@lextech.com>
2014-02-28 14:55:25 -0800

    Have prepare_command print command's stdout to console stdout.

--- lib/cocoapods/executable.rb
+++ lib/cocoapods/executable.rb
@@ -22,12 +22,12 @@ module Pod
     #
     def executable(name)
 
-      define_method(name) do |command|
-        Executable.execute_command(name, command, false)
+      define_method(name) do |command, output=false|
+        Executable.execute_command(name, command, output, false)
       end
 
-      define_method(name.to_s + "!") do |command|
-        Executable.execute_command(name, command, true)
+      define_method(name.to_s + "!") do |command, output=false|
+        Executable.execute_command(name, command, output, true)
       end
     end
 
@@ -50,7 +50,7 @@ module Pod
     #
     # @todo   Find a way to display the live output of the commands.
     #
-    def self.execute_command(executable, command, raise_on_failure)
+    def self.execute_command(executable, command, show_stdout, raise_on_failure)
 
       bin = `which #{executable}`.strip
       raise Informative, "Unable to locate the executable `#{executable}`" if bin.empty?
@@ -62,6 +62,9 @@ module Pod
       if Config.instance.verbose?
         UI.message("$ #{full_command}")
         stdout, stderr = Indenter.new(STDOUT), Indenter.new(STDERR)
+      elsif show_stdout
+        stdout, stderr = Indenter.new(STDOUT), Indenter.new(STDERR)
+        stdout.indent = 4
       else
         stdout, stderr = Indenter.new, Indenter.new
       end
