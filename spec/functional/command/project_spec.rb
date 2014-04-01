5e1ae41f70963018ad5e2668b423409be426540c
Marius Rackwitz <git@mariusrackwitz.de>
2014-04-01 13:17:19 +0200

    Changed spec as `pod update` falls back to `pod install` if no Lockfile is present
    Because it improves the user convenience and bundler does it the same way.

--- spec/functional/command/project_spec.rb
+++ spec/functional/command/project_spec.rb
@@ -34,9 +34,12 @@ module Pod
 
     it "tells the user that no Lockfile was found in the current working dir" do
       file = temporary_directory + 'Podfile'
-      File.open(file, 'w') {|f| f.write('platform :ios') }
+      File.open(file, 'w') do |f|
+        f.puts('platform :ios')
+        f.puts('pod "Reachability"')
+      end
       Dir.chdir(temporary_directory) do
-        exception = lambda { run_command('update','--no-repo-update') }.should.raise Informative
+        exception = lambda { run_command('update', 'Reachability', '--no-repo-update') }.should.raise Informative
         exception.message.should.include "No `Podfile.lock' found in the current working directory"
       end
     end
