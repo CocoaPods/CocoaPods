bf3ae9719b877df192a7f9e5b6e24d5695e33602
Samuel E. Giddins <segiddins@segiddins.me>
2014-04-15 11:10:27 -0500

    Refactored Command::Outdated to make it stub-able

--- lib/cocoapods/command/outdated.rb
+++ lib/cocoapods/command/outdated.rb
@@ -23,26 +23,6 @@ module Pod
       # @todo fix.
       #
       def run
-        verify_podfile_exists!
-        verify_lockfile_exists!
-
-        lockfile = config.lockfile
-        pods = lockfile.pod_names
-        updates = []
-        deprecated = []
-        pods.each do |pod_name|
-          set = SourcesManager.search(Dependency.new(pod_name))
-          next unless set
-          spec = set.specification
-          source_version = set.versions.first
-          lockfile_version = lockfile.version(pod_name)
-          if source_version > lockfile_version
-            updates << [pod_name, lockfile_version, source_version]
-          end
-          if spec.deprecated || spec.deprecated_in_favor_of
-            deprecated << spec
-          end
-        end
 
         if updates.empty?
           UI.puts "No updates are available.".yellow
@@ -54,12 +34,12 @@ module Pod
           end
         end
 
-        if deprecated.any?
+        if deprecated_pods.any?
           UI.section 'The following pods are deprecated:' do
-            deprecated.each do |spec|
+            deprecated_pods.each do |spec|
               if spec.deprecated_in_favor_of
-                UI.puts "- #{spec.name} in " \
-                  "favor of #{spec.deprecated_in_favor_of}"
+                UI.puts "- #{spec.name}" \
+                  " (in favor of #{spec.deprecated_in_favor_of})"
               else
                 UI.puts "- #{spec.name}"
               end
@@ -68,6 +48,56 @@ module Pod
         end
 
       end
+
+      private
+
+      def updates
+        @updates ||= begin
+          spec_sets.map do |set|
+            spec = set.specification
+            source_version = set.versions.first
+            pod_name = spec.name
+            lockfile_version = lockfile.version(pod_name)
+            if source_version > lockfile_version
+              [pod_name, lockfile_version, source_version]
+            else
+              nil
+            end
+          end.compact.uniq
+        end
+      end
+
+      def deprecated_pods
+        @deprecated_pods ||= begin
+          spec_sets.map(&:specification).select do |spec|
+            spec.deprecated || spec.deprecated_in_favor_of
+          end.compact.uniq
+        end
+      end
+
+      def spec_sets
+        @spec_sets ||= begin
+          installed_pods.map do |pod_name|
+            SourcesManager.search(Dependency.new(pod_name))
+          end.compact.uniq
+        end
+      end
+
+      def installed_pods
+        @installed_pods ||= begin
+          verify_podfile_exists!
+
+          lockfile.pod_names
+        end
+      end
+
+      def lockfile
+        @lockfile ||= begin
+          verify_lockfile_exists!
+          config.lockfile
+        end
+      end
+
     end
   end
 end
