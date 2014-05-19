8f74d0e4936b8ab3627c135cbdce9331b469fbf4
Fabio Pelosin <fabiopelosin@gmail.com>
2014-05-19 16:32:19 +0200

    [Command::Push] Add temporary foward command

--- lib/cocoapods/command.rb
+++ lib/cocoapods/command.rb
@@ -20,6 +20,9 @@ module Pod
     require 'cocoapods/command/spec'
     require 'cocoapods/command/init'
 
+    # TODO: remove
+    require 'cocoapods/command/push'
+
     self.abstract_command = true
     self.command = 'pod'
     self.version = VERSION
