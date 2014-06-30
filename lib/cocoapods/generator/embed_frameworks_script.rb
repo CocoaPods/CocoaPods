d752402ef6d9b7f0f1103f3359186df9a2a6130b
Marius Rackwitz <git@mariusrackwitz.de>
2014-06-30 13:34:52 +0200

    Add the Embed Pods Framework Build Phase in TargetIntegrator
    Embed frameworks with a run script build phase

---
+++ lib/cocoapods/generator/embed_frameworks_script.rb
@@ -0,0 +1,66 @@
+module Pod
+  module Generator
+    class EmbedFrameworksScript
+      # @return [Hash{String, Array{String}] Multiple lists of frameworks per
+      #         configuration.
+      #
+      attr_reader :frameworks_by_config
+
+      # @param  [Hash{String, Array{String}] frameworks_by_config
+      #         @see frameworks_by_config
+      #
+      def initialize(frameworks_by_config)
+        @frameworks_by_config = frameworks_by_config
+      end
+
+      # Saves the resource script to the given pathname.
+      #
+      # @param  [Pathname] pathname
+      #         The path where the embed frameworks script should be saved.
+      #
+      # @return [void]
+      #
+      def save_as(pathname)
+        pathname.open('w') do |file|
+          file.puts(script)
+        end
+        File.chmod(0755, pathname.to_s)
+      end
+
+      private
+
+      # @!group Private Helpers
+
+      # @return [String] The contents of the embed frameworks script.
+      #
+      def script
+        script = INSTALL_FRAMEWORKS_FUNCTION
+        script += "\n" unless frameworks_by_config.values.all?(&:empty?)
+        frameworks_by_config.each do |config, frameworks|
+          unless frameworks.empty?
+            script += %(if [[ "$CONFIGURATION" == "#{config}" ]]; then\n)
+            frameworks.each do |framework|
+              script += "  install_framework '#{framework}'\n"
+            end
+            script += "fi\n"
+          end
+        end
+        script
+      end
+
+      INSTALL_FRAMEWORKS_FUNCTION = <<EOS
+#!/bin/sh
+set -e
+
+echo "mkdir -p ${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+mkdir -p "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+
+install_framework()
+{
+  echo "rsync --exclude '*.h' -av ${PODS_ROOT}/$1 ${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+  rsync -av "${BUILT_PRODUCTS_DIR}/$1" "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+}
+EOS
+    end
+  end
+end
