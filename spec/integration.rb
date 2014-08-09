1775d4e26953aa500b570bce1f14115d61ec5271
Fabio Pelosin <fabiopelosin@gmail.com>
2014-08-09 18:49:17 +0200

    [Integration] Disable xcproj in Xcodeproj

--- spec/integration.rb
+++ spec/integration.rb
@@ -89,7 +89,8 @@ end
 #         environment.
 #
 def launch_binary(arguments, folder)
-  command = "CP_AGGRESSIVE_CACHE=TRUE #{POD_BINARY} #{arguments} --verbose --no-ansi 2>&1"
+  environment = 'CP_AGGRESSIVE_CACHE=TRUE XCODEPROJ_DISABLE_XCPROJ=TRUE '
+  command = "#{environment} #{POD_BINARY} #{arguments} --verbose --no-ansi 2>&1"
   Dir.chdir(TMP_DIR + folder) do
     output = `#{command}`
     it "$ pod #{arguments}" do
