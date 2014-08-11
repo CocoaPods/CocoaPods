03d5b793018f4e848e55f6e1106fe13a60df3474
Fabio Pelosin <fabiopelosin@gmail.com>
2014-08-11 20:19:39 +0200

    [HooksManager] Introduction

--- lib/cocoapods.rb
+++ lib/cocoapods.rb
@@ -40,6 +40,7 @@ module Pod
   autoload :Executable,                'cocoapods/executable'
   autoload :ExternalSources,           'cocoapods/external_sources'
   autoload :Installer,                 'cocoapods/installer'
+  autoload :HooksManager,              'cocoapods/hooks_manager'
   autoload :PodTarget,                 'cocoapods/target/pod_target'
   autoload :Project,                   'cocoapods/project'
   autoload :Resolver,                  'cocoapods/resolver'
---
+++ spec/unit/hooks_manager_spec.rb
@@ -0,0 +1,66 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+module Pod
+  describe HooksManager do
+    before do
+      @hooks_manager = Pod::HooksManager
+    end
+
+    describe 'register' do
+      it "allows to register a block for a notification with a given name" do
+        @hooks_manager.register(:post_install) {}
+        @hooks_manager.registrations[:post_install].count.should == 1
+        @hooks_manager.registrations[:post_install].first.class.should == Proc
+      end
+
+      it "raises if no name is given" do
+        should.raise ArgumentError do
+          @hooks_manager.register(nil) {}
+        end
+      end
+
+      it "raises if no block is given" do
+        should.raise ArgumentError do
+          @hooks_manager.register(:post_install)
+        end
+      end
+    end
+
+    describe 'run' do
+      it "invokes the hooks" do
+        @hooks_manager.register(:post_install) do |options|
+          true.should.be.true
+        end
+        @hooks_manager.run(:post_install, Object.new)
+      end
+
+      it "handles the case that no listeners have registered" do
+        should.not.raise do
+          @hooks_manager.run(:post_install, Object.new)
+        end
+      end
+
+      it "handles the case that no listeners have registered for a name" do
+        @hooks_manager.register(:post_install) do |options|
+          true.should.be.true
+        end
+        should.not.raise do
+          @hooks_manager.run(:pre_install, Object.new)
+        end
+      end
+
+      it "raises if no name is given" do
+        should.raise ArgumentError do
+          @hooks_manager.run(nil, Object.new) {}
+        end
+      end
+
+      it "raises if no context object is given" do
+        should.raise ArgumentError do
+          @hooks_manager.run(:post_install, nil)
+        end
+      end
+    end
+  end
+end
+
