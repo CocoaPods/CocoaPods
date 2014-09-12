c4453e4556024362895989a256585e8f484ca5b5
Kyle Fuller <inbox@kylefuller.co.uk>
2014-09-12 20:36:27 +0100

    [Intergrator] Fixes incorrect paths used when checking manifest
    Add extra ../ was recently introduced

--- spec/unit/installer/user_project_integrator/target_integrator_spec.rb
+++ spec/unit/installer/user_project_integrator/target_integrator_spec.rb
@@ -75,7 +75,7 @@ module Pod
           target = @target_integrator.send(:native_targets).first
           phase = target.shell_script_build_phases.find { |bp| bp.name == 'Check Pods Manifest.lock' }
           phase.shell_script.should == <<-EOS.strip_heredoc
-          diff "${PODS_ROOT}/../../Podfile.lock" "${PODS_ROOT}/Manifest.lock" > /dev/null
+          diff "${PODS_ROOT}/../Podfile.lock" "${PODS_ROOT}/Manifest.lock" > /dev/null
           if [[ $? != 0 ]] ; then
               cat << EOM
           error: The sandbox is not in sync with the Podfile.lock. Run 'pod install' or update your CocoaPods installation.
