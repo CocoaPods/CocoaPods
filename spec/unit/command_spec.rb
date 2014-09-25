f3803f4c8fe21fb3a21d452aec0443431637649a
Samuel E. Giddins <segiddins@segiddins.me>
2014-09-25 11:11:15 -0700

    [CommandSpec] Expect pod repo to default to pod repo list

--- spec/unit/command_spec.rb
+++ spec/unit/command_spec.rb
@@ -6,7 +6,7 @@ module Pod
       Command.parse(%w(install      )).should.be.instance_of Command::Install
       Command.parse(%w(list         )).should.be.instance_of Command::List
       Command.parse(%w(outdated     )).should.be.instance_of Command::Outdated
-      Command.parse(%w(repo         )).should.be.instance_of Command::Repo
+      Command.parse(%w(repo         )).should.be.instance_of Command::Repo::List
       Command.parse(%w(repo add     )).should.be.instance_of Command::Repo::Add
       Command.parse(%w(repo lint    )).should.be.instance_of Command::Repo::Lint
       Command.parse(%w(repo list    )).should.be.instance_of Command::Repo::List
