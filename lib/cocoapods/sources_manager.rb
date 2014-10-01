09cacf8c8c3a1ab0ac37872be377432cb0ceecce
Erik Ackermann <root@erikackermann.com>
2014-10-01 15:24:20 -0400

    Remove trailing '\n' character in update prompt
    This character isn't needed at the end of the string.  Having the '\n' shows up in the terminal.

--- lib/cocoapods/sources_manager.rb
+++ lib/cocoapods/sources_manager.rb
@@ -246,7 +246,7 @@ module Pod
           install_message << 'gem install cocoapods'
           install_message << ' --pre' if Gem::Version.new(last).prerelease?
           UI.puts "\nCocoaPods #{versions['last']} is available.\n" \
-            "To update use: `#{install_message}`".green + "\n"
+            "To update use: `#{install_message}`".green
         end
       end
 
