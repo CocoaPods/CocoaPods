825553eb21610726c040121da58434620f4f72ac
Marius Rackwitz <git@mariusrackwitz.de>
2014-10-29 21:43:19 +0100

    [Spec] Mute leaking output of the spec for Command::Repo::Update

--- spec/spec_helper/temporary_repos.rb
+++ spec/spec_helper/temporary_repos.rb
@@ -30,7 +30,7 @@ module SpecHelper
     # Clones a repo to the given name.
     #
     def repo_clone(from_name, to_name)
-      Dir.chdir(tmp_repos_path) { `git clone #{from_name} #{to_name}` }
+      Dir.chdir(tmp_repos_path) { `git clone #{from_name} #{to_name} 2>&1 > /dev/null` }
       repo_path(to_name)
     end
 
