9ffeedc9058bc4f3fd0fc7ec7e0a420efa257d15
Marius Rackwitz <git@mariusrackwitz.de>
2014-11-06 13:15:54 +0100

    Add helper method Target#c99ext_identifier

--- spec/unit/target_spec.rb
+++ spec/unit/target_spec.rb
@@ -3,5 +3,20 @@ require File.expand_path('../../spec_helper', __FILE__)
 module Pod
   describe Target do
 
+    describe '#c99ext_identifier' do
+      before do
+        @target = Target.new
+      end
+
+      it 'should mask, but keep leading numbers' do
+        @target.send(:c99ext_identifier, '123BananaLib').should == '_123BananaLib'
+      end
+
+      it 'should mask invalid chars' do
+        @target.send(:c99ext_identifier, 'iOS-App BânánàLïb').should == 'iOS_App_B_n_n_L_b'
+      end
+
+    end
+
   end
 end
