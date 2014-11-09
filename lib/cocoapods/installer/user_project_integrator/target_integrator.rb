660531b1869c14a2ceb25242056dc72b60e41696
Marius Rackwitz <git@mariusrackwitz.de>
2014-11-09 17:06:42 +0100

    Weak link the aggregate framework
    The aggregate target's product isn't really needed, so it isn't copied into the app bundle to not clutter and expose details of the build process. `dyld` is trying to link it at runtime, as the aggregate framework is linked to the user's target to trick Xcode, so that it concludes there is target dependency without the need to declare a literal target dependency, which implies that we would need to add a subproject and add the reference to a target, which is newly created on each run of `pod install`, which would cause again undesirably changes to the user's project.

--- lib/cocoapods/installer/user_project_integrator/target_integrator.rb
+++ lib/cocoapods/installer/user_project_integrator/target_integrator.rb
@@ -117,8 +117,12 @@ module Pod
             target_basename = target.product_basename
             new_product_ref = frameworks.files.find { |f| f.path == target.product_name } ||
               frameworks.new_product_ref_for_target(target_basename, target.product_type)
-            unless build_phase.files_references.include?(new_product_ref)
-              build_phase.add_file_reference(new_product_ref)
+            unless build_file = build_phase.build_file(new_product_ref)
+              build_file = build_phase.add_file_reference(new_product_ref)
+            end
+            if target.requires_framework?
+              build_file.settings ||= {}
+              build_file.settings['ATTRIBUTES'] = ['Weak']
             end
           end
         end
