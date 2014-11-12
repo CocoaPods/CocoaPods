2e6a2c9e411d68d55ca6e719801939ab58b3fb17
Samuel E. Giddins <segiddins@segiddins.me>
2014-11-12 12:29:42 -0800

    [Analyzer] Ensure that the right checkout options are used when a downlaod of the Pod already exists

--- lib/cocoapods/installer/analyzer.rb
+++ lib/cocoapods/installer/analyzer.rb
@@ -265,17 +265,23 @@ module Pod
       def fetch_external_sources
         return unless allow_pre_downloads?
 
-        deps_with_different_sources = podfile.dependencies.group_by(&:root_name).select { |_root_name, dependencies| dependencies.map(&:external_source).uniq.count > 1 }
-        deps_with_different_sources.each do |root_name, dependencies|
-          raise Informative, "There are multiple dependencies with different sources for `#{root_name}` in #{UI.path podfile.defined_in_file}:\n\n- #{dependencies.map(&:to_s).join("\n- ")}"
-        end
-
+        verify_no_pods_with_different_sources!
         unless dependencies_to_fetch.empty?
           UI.section 'Fetching external sources' do
             dependencies_to_fetch.sort.each do |dependency|
               fetch_external_source(dependency, !pods_to_fetch.include?(dependency.name))
             end
           end
+        end
+      end
+
+      def verify_no_pods_with_different_sources!
+        deps_with_different_sources = podfile.dependencies.group_by(&:root_name).
+          select { |_root_name, dependencies| dependencies.map(&:external_source).uniq.count > 1 }
+        deps_with_different_sources.each do |root_name, dependencies|
+          raise Informative, "There are multiple dependencies with different " \
+          "sources for `#{root_name}` in #{UI.path podfile.defined_in_file}:" \
+          "\n\n- #{dependencies.map(&:to_s).join("\n- ")}"
         end
       end
 
@@ -300,10 +306,23 @@ module Pod
           else
             deps_to_fetch = deps_with_external_source.select { |dep| pods_to_fetch.include?(dep.name) }
             deps_to_fetch_if_needed = deps_with_external_source.select { |dep| result.podfile_state.unchanged.include?(dep.name) }
-            deps_to_fetch += deps_to_fetch_if_needed.select { |dep| sandbox.specification(dep.name).nil? || !dep.external_source[:local].nil? || !dep.external_source[:path].nil? || !sandbox.pod_dir(dep.name).directory? }
+            deps_to_fetch += deps_to_fetch_if_needed.select do |dep|
+              sandbox.specification(dep.name).nil? ||
+              !dep.external_source[:local].nil? ||
+              !dep.external_source[:path].nil? ||
+              !sandbox.pod_dir(dep.root_name).directory? ||
+              checkout_requires_update?(dep)
+            end
           end
           deps_to_fetch.uniq(&:root_name)
         end
+      end
+
+      def checkout_requires_update?(dependency)
+        return true unless lockfile && sandbox.manifest
+        locked_checkout_options = lockfile.checkout_options_for_pod_named(dependency.root_name)
+        sandbox_checkout_options = sandbox.manifest.checkout_options_for_pod_named(dependency.root_name)
+        locked_checkout_options != sandbox_checkout_options
       end
 
       def pods_to_fetch
