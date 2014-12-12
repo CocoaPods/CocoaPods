e244568fcf15a1bd4664d3fa8959703cb316dbad
Boris Bügling <boris@icculus.org>
2014-12-12 14:29:25 +0100

    Fix typo of variable name in Installer.

--- lib/cocoapods/installer.rb
+++ lib/cocoapods/installer.rb
@@ -484,7 +484,7 @@ module Pod
               pod_target.native_target.add_dependency(pod_dependency_target.native_target)
 
               if pod_target.requires_frameworks?
-                product_ref = frameworks_group.files.find { |f| f.path == pod_dep_target.product_name } ||
+                product_ref = frameworks_group.files.find { |f| f.path == pod_dependency_target.product_name } ||
                   frameworks_group.new_product_ref_for_target(pod_dependency_target.product_basename, pod_dependency_target.product_type)
                 pod_target.native_target.frameworks_build_phase.add_file_reference(product_ref)
               end
