8349b8a6c178ae20b94e73a4981c6e16b8a9a89a
Samuel E. Giddins <segiddins@segiddins.me>
2015-02-18 11:38:19 -0800

    Update for changes to grepping in the embed frameworks script

--- install_framework_resources/after/Pods/Target Support Files/Pods-SampleApp/Pods-SampleApp-frameworks.sh
+++ install_framework_resources/after/Pods/Target Support Files/Pods-SampleApp/Pods-SampleApp-frameworks.sh
@@ -28,7 +28,7 @@ install_framework()
   local basename
   basename=$(echo $1 | sed -E s/\\..+// && exit ${PIPESTATUS[0]})
   local swift_runtime_libs
-  swift_runtime_libs=$(xcrun otool -LX "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}/$1/${basename}" | grep @rpath/libswift | sed -E s/@rpath\\/\(.+dylib\).*/\\1/g | uniq -u  && exit ${PIPESTATUS[0]})
+  swift_runtime_libs=$(xcrun otool -LX "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}/$1/${basename}" | grep --color=never @rpath/libswift | sed -E s/@rpath\\/\(.+dylib\).*/\\1/g | uniq -u  && exit ${PIPESTATUS[0]})
   for lib in $swift_runtime_libs; do
     echo "rsync -av \"${SWIFT_STDLIB_PATH}/${lib}\" \"${destination}\""
     rsync -av "${SWIFT_STDLIB_PATH}/${lib}" "${destination}"
