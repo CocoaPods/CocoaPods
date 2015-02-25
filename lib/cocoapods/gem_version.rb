a06299dc563855c10648fe8881c3f410e5fa608d
Samuel E. Giddins <segiddins@segiddins.me>
2015-02-24 21:10:32 -0800

    Release 0.36.0.rc.1

--- lib/cocoapods/gem_version.rb
+++ lib/cocoapods/gem_version.rb
@@ -1,5 +1,5 @@
 module Pod
   # The version of the cocoapods command line tool.
   #
-  VERSION = '0.36.0.beta.2' unless defined? Pod::VERSION
+  VERSION = '0.36.0.rc.1' unless defined? Pod::VERSION
 end
