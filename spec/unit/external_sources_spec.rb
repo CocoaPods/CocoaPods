fc23760c30cab8cb2c0aa23cca972c5bc3e02b60
Samuel E. Giddins <segiddins@segiddins.me>
2015-03-31 20:02:53 -0700

    [ExternalSources] Fix specs for compacting of external sources hash

--- spec/unit/external_sources_spec.rb
+++ spec/unit/external_sources_spec.rb
@@ -8,26 +8,26 @@ module Pod
 
     describe 'from_dependency' do
       it 'supports a podspec source' do
-        dep = Dependency.new('Reachability', :podspec => nil)
+        dep = Dependency.new('Reachability', :podspec => '')
         klass = @subject.from_dependency(dep, nil).class
         klass.should == @subject::PodspecSource
       end
 
       it 'supports a path source' do
-        dep = Dependency.new('Reachability', :path => nil)
+        dep = Dependency.new('Reachability', :path => '')
         klass = @subject.from_dependency(dep, nil).class
         klass.should == @subject::PathSource
       end
 
       it 'supports a path source specified with the legacy :local key' do
-        dep = Dependency.new('Reachability', :local => nil)
+        dep = Dependency.new('Reachability', :local => '')
         klass = @subject.from_dependency(dep, nil).class
         klass.should == @subject::PathSource
       end
 
       it 'supports all the strategies implemented by the downloader' do
         [:git, :svn, :hg, :bzr, :http].each do |strategy|
-          dep     = Dependency.new('Reachability', strategy => nil)
+          dep     = Dependency.new('Reachability', strategy => '')
           klass = @subject.from_dependency(dep, nil).class
           klass.should == @subject::DownloaderSource
         end
