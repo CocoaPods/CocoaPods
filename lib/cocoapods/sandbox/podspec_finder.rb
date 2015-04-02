538b2d2e32ce3f0301aeee35373e24fe61020e7a
Samuel E. Giddins <segiddins@segiddins.me>
2015-04-01 21:22:28 -0700

    [PodspecFinder] Use pathname for globbing

--- lib/cocoapods/sandbox/podspec_finder.rb
+++ lib/cocoapods/sandbox/podspec_finder.rb
@@ -10,13 +10,13 @@ module Pod
       def podspecs
         return @specs_by_name if @specs_by_name
         @specs_by_name = {}
-        spec_files = Dir.glob(root + '{,*,*/*}.podspec{,.json}')
-        spec_files.sort_by { |p| -p.split(File::SEPARATOR).size }.each do |file|
+        spec_files = Pathname.glob(root + '{,*,*/*}.podspec{,.json}')
+        spec_files.sort_by { |p| -p.to_path.split(File::SEPARATOR).size }.each do |file|
           begin
             spec = Specification.from_file(file)
             @specs_by_name[spec.name] = spec
           rescue => e
-            UI.warn "Unable to load a podspec from #{file}, skipping:\n\n#{e}"
+            UI.warn "Unable to load a podspec from `#{file.basename}`, skipping:\n\n#{e}"
           end
         end
         @specs_by_name
