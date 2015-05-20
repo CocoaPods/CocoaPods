fc19c0e943319ff473d87c3d725e68a3c608b7a6
Samuel E. Giddins <segiddins@segiddins.me>
2015-05-19 22:15:03 -0500

    [Cache] Ensure we handle when the tmpdir cant be created
    Closes #3587.

--- lib/cocoapods/downloader/cache.rb
+++ lib/cocoapods/downloader/cache.rb
@@ -127,7 +127,7 @@ module Pod
         tmpdir = Pathname(Dir.mktmpdir)
         blk.call(tmpdir)
       ensure
-        FileUtils.remove_entry(tmpdir) if tmpdir.exist?
+        FileUtils.remove_entry(tmpdir) if tmpdir && tmpdir.exist?
       end
 
       # Copies the `source` directory to `destination`, cleaning the directory
