b8dc448b3a55b203eda13ad8bc786226a811848c
Tim Rosenblatt <tim@ship.io>
2015-06-11 12:26:30 -0700

    bugfix for pods containing swift frameworks

--- lib/cocoapods/generator/embed_frameworks_script.rb
+++ lib/cocoapods/generator/embed_frameworks_script.rb
@@ -45,7 +45,12 @@ module Pod
 
           install_framework()
           {
-            local source="${BUILT_PRODUCTS_DIR}/$1"
+            if [ -r "${BUILT_PRODUCTS_DIR}/#{target_definition.label}/$1" ]; then
+              local source="${BUILT_PRODUCTS_DIR}/#{target_definition.label}/$1"
+            else
+              local source="${BUILT_PRODUCTS_DIR}/$1"
+            fi
+
             local destination="${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
 
             if [ -L "${source}" ]; then
