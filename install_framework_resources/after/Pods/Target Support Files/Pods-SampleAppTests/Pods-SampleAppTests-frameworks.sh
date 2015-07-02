6d4b38764f551fcda4da634ba777640d96162b56
Samuel E. Giddins <segiddins@segiddins.me>
2015-07-01 19:55:34 -0700

    Add more quotes to the embed frameworks script

--- install_framework_resources/after/Pods/Target Support Files/Pods-SampleAppTests/Pods-SampleAppTests-frameworks.sh
+++ install_framework_resources/after/Pods/Target Support Files/Pods-SampleAppTests/Pods-SampleAppTests-frameworks.sh
@@ -18,11 +18,11 @@ install_framework()
 
   if [ -L "${source}" ]; then
       echo "Symlinked..."
-      source=$(readlink "${source}")
+      source="$(readlink "${source}")"
   fi
 
   # use filter instead of exclude so missing patterns dont' throw errors
-  echo "rsync -av --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" ${source} ${destination}"
+  echo "rsync -av --filter \"- CVS/\" --filter \"- .svn/\" --filter \"- .git/\" --filter \"- .hg/\" --filter \"- Headers\" --filter \"- PrivateHeaders\" --filter \"- Modules\" \"${source}\" \"${destination}\""
   rsync -av --filter "- CVS/" --filter "- .svn/" --filter "- .git/" --filter "- .hg/" --filter "- Headers" --filter "- PrivateHeaders" --filter "- Modules" "${source}" "${destination}"
 
   # Resign the code if required by the build settings to avoid unstable apps
@@ -30,7 +30,7 @@ install_framework()
 
   # Embed linked Swift runtime libraries
   local basename
-  basename=$(basename $1 | sed -E s/\\..+// && exit ${PIPESTATUS[0]})
+  basename="$(basename "$1" | sed -E s/\\..+// && exit ${PIPESTATUS[0]})"
   local swift_runtime_libs
   swift_runtime_libs=$(xcrun otool -LX "${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}/${basename}.framework/${basename}" | grep --color=never @rpath/libswift | sed -E s/@rpath\\/\(.+dylib\).*/\\1/g | uniq -u  && exit ${PIPESTATUS[0]})
   for lib in $swift_runtime_libs; do
@@ -45,7 +45,7 @@ code_sign_if_enabled() {
   if [ -n "${EXPANDED_CODE_SIGN_IDENTITY}" -a "${CODE_SIGNING_REQUIRED}" != "NO" -a "${CODE_SIGNING_ALLOWED}" != "NO" ]; then
     # Use the current code_sign_identitiy
     echo "Code Signing $1 with Identity ${EXPANDED_CODE_SIGN_IDENTITY_NAME}"
-    echo "/usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --preserve-metadata=identifier,entitlements $1"
+    echo "/usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --preserve-metadata=identifier,entitlements \"$1\""
     /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --preserve-metadata=identifier,entitlements "$1"
   fi
 }
