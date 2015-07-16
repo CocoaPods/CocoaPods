7d8ecf6481afe04fd5242b213c02c28ddb6b7927
Samuel E. Giddins <segiddins@segiddins.me>
2015-07-15 22:29:11 -0700

    [BridgeSupportSpec] Fix execution

--- spec/unit/generator/bridge_support_spec.rb
+++ spec/unit/generator/bridge_support_spec.rb
@@ -1,15 +1,17 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
-describe 'Pod::Generator::BridgeSupport' do
-  if Executable.which('gen_bridge_metadata').nil?
-    puts '  ! '.red << "Skipping because the `gen_bridge_metadata` executable can't be found."
-  else
-    it 'generates a metadata file with the appropriate search paths' do
-      headers = %w(/some/dir/foo.h /some/dir/bar.h /some/other/dir/baz.h).map { |h| Pathname.new(h) }
-      generator = Pod::Generator::BridgeSupport.new(headers)
-      expected = ['-c', "-I '/some/dir' -I '/some/other/dir'", '-o', '/path/to/Pods.bridgesupport', *headers]
-      generator.expects(:gen_bridge_metadata).with { |*command| command.map(&:to_s) == expected.map(&:to_s) }
-      generator.save_as(Pathname.new('/path/to/Pods.bridgesupport'))
+module Pod
+  describe 'Pod::Generator::BridgeSupport' do
+    if Executable.which('gen_bridge_metadata').nil?
+      puts '  ! '.red << "Skipping because the `gen_bridge_metadata` executable can't be found."
+    else
+      it 'generates a metadata file with the appropriate search paths' do
+        headers = %w(/some/dir/foo.h /some/dir/bar.h /some/other/dir/baz.h).map { |h| Pathname.new(h) }
+        generator = Generator::BridgeSupport.new(headers)
+        expected = ['-c', "-I '/some/dir' -I '/some/other/dir'", '-o', '/path/to/Pods.bridgesupport', *headers]
+        generator.expects(:gen_bridge_metadata).with { |*command| command.map(&:to_s) == expected.map(&:to_s) }
+        generator.save_as(Pathname.new('/path/to/Pods.bridgesupport'))
+      end
     end
   end
 end
