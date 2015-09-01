e5d504032e2af0f41b4662034f8637dba5351da9
Emma Koszinowski <emkosz@gmail.com>
2015-09-01 16:12:56 -0700

    Removed reference to Command::Search

--- spec/unit/command_spec.rb
+++ spec/unit/command_spec.rb
@@ -12,7 +12,6 @@ module Pod
       Command.parse(%w(repo list    )).should.be.instance_of Command::Repo::List
       Command.parse(%w(repo update  )).should.be.instance_of Command::Repo::Update
       Command.parse(%w(repo remove  )).should.be.instance_of Command::Repo::Remove
-      Command.parse(%w(search       )).should.be.instance_of Command::Search
       Command.parse(%w(setup        )).should.be.instance_of Command::Setup
       Command.parse(%w(spec create  )).should.be.instance_of Command::Spec::Create
       Command.parse(%w(spec lint    )).should.be.instance_of Command::Spec::Lint
