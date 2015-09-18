9fc34261668aed86df60bc555aead0398dc6464d
Jamie Riley Evans <jamie.riley.evans@gmail.com>
2015-09-18 14:11:39 -0400

    Added `source` and valuable flags to Podfile template
    All Podfiles should specify a `source` and with the growing number of Swift applications, having the `use_frameworks!` flag available is also very helpful.
    
    Also, including the `inhibit_all_warnings!` will usually save a google search, as well. Most people haven't memorized all of the common cocoapod flags.

--- lib/cocoapods/command/init.rb
+++ lib/cocoapods/command/init.rb
@@ -56,8 +56,13 @@ module Pod
         podfile = ''
         podfile << "xcodeproj '#{@project_path}'\n\n" if @project_path
         podfile << <<-PLATFORM.strip_heredoc
+          source 'https://github.com/CocoaPods/Specs.git'
           # Uncomment this line to define a global platform for your project
           # platform :ios, '6.0'
+          # Uncomment this line if you're using Swift
+          # use_frameworks!
+          # Uncomment this line to ignore all warnings from all pods
+          # inhibit_all_warnings!
         PLATFORM
 
         project.targets.each do |target|
