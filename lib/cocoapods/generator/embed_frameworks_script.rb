e98cab259a57267996910f2a3167d52e7a4a5124
DJ Tarazona <me@djtarazona.com>
2015-09-19 21:13:10 -0700

    Remove copying of Swift runtime libraries
    Fixes `Invalid Swift Support` error when uploading to iTunes Connect with Xcode 7.

--- lib/cocoapods/generator/embed_frameworks_script.rb
+++ lib/cocoapods/generator/embed_frameworks_script.rb
@@ -78,15 +78,6 @@ module Pod
 
             # Resign the code if required by the build settings to avoid unstable apps
             code_sign_if_enabled "${destination}/$(basename "$1")"
-
-            # Embed linked Swift runtime libraries
-            local swift_runtime_libs
-            swift_runtime_libs=$(xcrun otool -LX "$binary" | grep --color=never @rpath/libswift | sed -E s/@rpath\\\\/\\(.+dylib\\).*/\\\\1/g | uniq -u  && exit ${PIPESTATUS[0]})
-            for lib in $swift_runtime_libs; do
-              echo "rsync -auv \\"${SWIFT_STDLIB_PATH}/${lib}\\" \\"${destination}\\""
-              rsync -auv "${SWIFT_STDLIB_PATH}/${lib}" "${destination}"
-              code_sign_if_enabled "${destination}/${lib}"
-            done
           }
 
           # Signs a framework with the provided identity
