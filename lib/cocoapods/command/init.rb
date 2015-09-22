4469a1727015c64fc39737322ae46d81caa637d1
Jamie Riley Evans <jamie.riley.evans@gmail.com>
2015-09-22 10:09:15 -0400

    Removed the `inhibit_all_warnings!` and `source` lines from the Podfile initializer.

--- lib/cocoapods/command/init.rb
+++ lib/cocoapods/command/init.rb
@@ -56,13 +56,10 @@ module Pod
         podfile = ''
         podfile << "xcodeproj '#{@project_path}'\n\n" if @project_path
         podfile << <<-PLATFORM.strip_heredoc
-          source 'https://github.com/CocoaPods/Specs.git'
           # Uncomment this line to define a global platform for your project
-          # platform :ios, '6.0'
+          # platform :ios, '8.0'
           # Uncomment this line if you're using Swift
           # use_frameworks!
-          # Uncomment this line to ignore all warnings from all pods
-          # inhibit_all_warnings!
         PLATFORM
 
         project.targets.each do |target|
