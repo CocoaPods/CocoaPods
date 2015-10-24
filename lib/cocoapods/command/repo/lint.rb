f15f65f7519751bca27e6b9bb093f952df42f907
Muhammed Yavuz Nuzumlali <manuyavuz@gmail.com>
2015-10-24 15:57:51 +0300

    Use SourceManager methods in repo lint to get sources

--- lib/cocoapods/command/repo/lint.rb
+++ lib/cocoapods/command/repo/lint.rb
@@ -35,15 +35,16 @@ module Pod
         #
         def run
           if @name
-            dirs = File.exist?(@name) ? [Pathname.new(@name)] : [dir]
+            sources = SourcesManager.sources([@name])
           else
-            dirs = config.repos_dir.children.select(&:directory?)
+            sources = SourcesManager.aggregate.sources
           end
-          dirs.each do |dir|
-            SourcesManager.check_version_information(dir)
-            UI.puts "\nLinting spec repo `#{dir.realpath.basename}`\n".yellow
 
-            validator = Source::HealthReporter.new(dir)
+          sources.each do |source|
+            SourcesManager.check_version_information(source.repo)
+            UI.puts "\nLinting spec repo `#{source.repo.basename}`\n".yellow
+
+            validator = Source::HealthReporter.new(source.repo)
             validator.pre_check do |_name, _version|
               UI.print '.'
             end
