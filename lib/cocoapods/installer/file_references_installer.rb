5b6ec485c07ff46fd4f54684080491e5b8ffdcf2
Eric Firestone <firestone@squareup.com>
2015-12-05 02:19:02 -0800

    Fix issues with variant group, versioned, and asset catalog resources

--- lib/cocoapods/installer/file_references_installer.rb
+++ lib/cocoapods/installer/file_references_installer.rb
@@ -177,11 +177,60 @@ module Pod
           pod_name = file_accessor.spec.name
           local = sandbox.local?(pod_name)
           paths = file_accessor.send(file_accessor_key)
+          paths = allowable_project_paths(paths)
           paths.each do |path|
             group = pods_project.group_for_spec(file_accessor.spec.name, group_key)
             pods_project.add_file_reference(path, group, local && reflect_file_system_structure_for_development)
           end
         end
+      end
+
+      # Filters a list of paths down to those paths which can be added to
+      # the Xcode project. Some paths are intermediates and only their children
+      # should be added, while some paths are treated as bundles and their
+      # children should not be added directly.
+      #
+      # @param  [Array<Pathname>] paths
+      #         The paths to files or directories on disk.
+      #
+      # @return [Array<Pathname>] The paths which can be added to the Xcode project
+      #
+      def allowable_project_paths(paths)
+        lproj_paths = Set.new
+        lproj_paths_with_files = Set.new
+        allowable_paths = paths.select do |path|
+          path_str = path.to_s.downcase
+
+          # We add the directory for a Core Data model, but not the items in it.
+          next if path_str =~ /.*\.xcdatamodeld\/.+/
+
+          # We add the directory for an asset catalog, but not the items in it.
+          next if path_str =~ /.*\.xcassets\/.+/
+
+          if path_str =~ /\.lproj(\/|$)/
+            # If the element is an .lproj directory then save it and potentially
+            # add it later if we don't find any contained items.
+            if path_str.end_with?('.lproj') && path.directory?
+              lproj_paths << path_str
+              next
+            end
+
+            # Collect the paths for the .lproj directories that contain files.
+            lproj_path = /(^.*\.lproj)\/.*/.match(path_str)[1]
+            lproj_paths_with_files << lproj_path
+
+            # Directories nested within an .lproj directory are added as file
+            # system references so their contained items are not added directly.
+            next if path.dirname.dirname.to_s.downcase == lproj_path
+          end
+
+          true
+        end
+
+        # Only add the path for the .lproj directories that do not have anything
+        # within them added as well. This generally happens if the glob within the
+        # resources directory was not a recursive glob.
+        allowable_paths + lproj_paths.subtract(lproj_paths_with_files).to_a
       end
 
       # Computes the destination sub-directory in the sandbox
