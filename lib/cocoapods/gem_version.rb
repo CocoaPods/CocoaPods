0d0f454dad40720175b8e7340650e055f83a3264
Samuel Giddins <segiddins@segiddins.me>
2015-12-30 16:04:28 -0500

    [GemVersion] Freeze the version string

--- lib/cocoapods/gem_version.rb
+++ lib/cocoapods/gem_version.rb
@@ -1,5 +1,5 @@
 module Pod
-  # The version of the cocoapods command line tool.
+  # The version of the CocoaPods command line tool.
   #
-  VERSION = '0.39.0' unless defined? Pod::VERSION
+  VERSION = '0.39.0'.freeze unless defined? Pod::VERSION
 end
