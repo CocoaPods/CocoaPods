231c8fc93b5ab52c79f95a211a99059d596a6af2
Marius Rackwitz <git@mariusrackwitz.de>
2016-03-11 13:51:50 +0100

    Rebuild for CocoaPods/CocoaPods#5025

--- install_subspecs/after/Pods/Target Support Files/Pods-OS X App/Pods-OS X App-frameworks.sh
+++ install_subspecs/after/Pods/Target Support Files/Pods-OS X App/Pods-OS X App-frameworks.sh
@@ -16,7 +16,7 @@ install_framework()
     local source="$1"
   fi
 
-  local destination="${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+  local destination="${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
 
   if [ -L "${source}" ]; then
       echo "Symlinked..."
@@ -84,8 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$CONFIGURATION_BUILD_DIR/PodTest-subspec_2/PodTest.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PodTest-subspec_2/PodTest.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$CONFIGURATION_BUILD_DIR/PodTest-subspec_2/PodTest.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PodTest-subspec_2/PodTest.framework"
 fi
--- install_vendored_dynamic_framework/after/Pods/Target Support Files/Pods-SampleApp/Pods-SampleApp-frameworks.sh
+++ install_vendored_dynamic_framework/after/Pods/Target Support Files/Pods-SampleApp/Pods-SampleApp-frameworks.sh
@@ -16,7 +16,7 @@ install_framework()
     local source="$1"
   fi
 
-  local destination="${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+  local destination="${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
 
   if [ -L "${source}" ]; then
       echo "Symlinked..."
@@ -85,9 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${PODS_ROOT}/Realm/realm-swift-0.94.1/ios/swift-1.2/Realm.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/RealmSwift/RealmSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${PODS_ROOT}/Realm/realm-swift-0.94.1/ios/swift-1.2/Realm.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/RealmSwift/RealmSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
 fi
