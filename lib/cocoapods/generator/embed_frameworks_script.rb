cc9a88a45cfcd099924cb6654f5ef6a7a0168f9f
Václav Slavík <vaclav@slavik.io>
2016-03-25 18:40:43 +0100

    Respect OTHER_CODE_SIGN_FLAGS setting
    Pass the OTHER_CODE_SIGN_FLAGS variable (aka "Other Code Signing Flags")
    to codesign, as Xcode does.

--- lib/cocoapods/generator/embed_frameworks_script.rb
+++ lib/cocoapods/generator/embed_frameworks_script.rb
@@ -96,8 +96,8 @@ module Pod
             if [ -n "${EXPANDED_CODE_SIGN_IDENTITY}" -a "${CODE_SIGNING_REQUIRED}" != "NO" -a "${CODE_SIGNING_ALLOWED}" != "NO" ]; then
               # Use the current code_sign_identitiy
               echo "Code Signing $1 with Identity ${EXPANDED_CODE_SIGN_IDENTITY_NAME}"
-              echo "/usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --preserve-metadata=identifier,entitlements \\"$1\\""
-              /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} --preserve-metadata=identifier,entitlements "$1"
+              echo "/usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} ${OTHER_CODE_SIGN_FLAGS} --preserve-metadata=identifier,entitlements \\"$1\\""
+              /usr/bin/codesign --force --sign ${EXPANDED_CODE_SIGN_IDENTITY} ${OTHER_CODE_SIGN_FLAGS} --preserve-metadata=identifier,entitlements "$1"
             fi
           }
 
