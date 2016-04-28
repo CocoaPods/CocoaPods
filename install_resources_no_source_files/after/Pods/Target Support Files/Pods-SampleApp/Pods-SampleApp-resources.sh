ca06326726ac1da9821372847cba9691fb652156
Marius Rackwitz <git@mariusrackwitz.de>
2016-04-28 20:39:51 +0200

    Rebuild for CocoaPods/CocoaPods#5222

--- install_resources_no_source_files/after/Pods/Target Support Files/Pods-SampleApp/Pods-SampleApp-resources.sh
+++ install_resources_no_source_files/after/Pods/Target Support Files/Pods-SampleApp/Pods-SampleApp-resources.sh
@@ -81,11 +81,11 @@ EOM
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "../res/troll.png"
-  install_resource "$CONFIGURATION_BUILD_DIR/res/res.bundle"
+  install_resource "$PODS_CONFIGURATION_BUILD_DIR/res/res.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "../res/troll.png"
-  install_resource "$CONFIGURATION_BUILD_DIR/res/res.bundle"
+  install_resource "$PODS_CONFIGURATION_BUILD_DIR/res/res.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
