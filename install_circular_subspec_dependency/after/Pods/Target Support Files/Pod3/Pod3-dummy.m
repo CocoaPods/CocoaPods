409958a75378ff70582af2fbabb1441c551ed1c7
Juan Pablo Civile <elementohb@gmail.com>
2016-06-03 11:24:18 -0300

    [PodTarget] Add a cyclic dependencies between subspecs case

---
+++ install_circular_subspec_dependency/after/Pods/Target Support Files/Pod3/Pod3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pod3 : NSObject
+@end
+@implementation PodsDummy_Pod3
+@end
---
+++ install_circular_subspec_dependency/before/DeveloperPods/Pod2/Pod2.h
@@ -0,0 +1,13 @@
+//
+//  Pod2.h
+//  Pods
+//
+//  Created by Peter Wiesner on 19/05/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pod2 : NSObject
+
+@end
