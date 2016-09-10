1eaaf63b3288eb93e3669c0a20589b185bdbc1f5
Danielle Tomlinson <dan@tomlinson.io>
2016-09-10 21:00:27 +0200

    Release 1.1.0.rc.1

--- lib/cocoapods/gem_version.rb
+++ lib/cocoapods/gem_version.rb
@@ -1,5 +1,5 @@
 module Pod
   # The version of the CocoaPods command line tool.
   #
-  VERSION = '1.1.0.beta.2'.freeze unless defined? Pod::VERSION
+  VERSION = '1.1.0.rc.1'.freeze unless defined? Pod::VERSION
 end
