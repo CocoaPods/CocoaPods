d7b1a9c0a9fa94c7049893b7205db291c8a307e1
Stephen Hayes <shayes20@its.jnj.com>
2016-10-02 20:11:10 -0400

    Updated for rubocop

--- lib/cocoapods/installer.rb
+++ lib/cocoapods/installer.rb
@@ -480,10 +480,10 @@ module Pod
       pods_installed = root_specs.size
       title_options = { :verbose_prefix => '-> '.green }
       UI.titled_section('Pod installation complete! ' \
-              "There #{podfile_dependencies == 1 ? 'is' : 'are'} #{podfile_dependencies} " \
-              "#{'dependency'.pluralize(podfile_dependencies)} from the Podfile " \
-              "and #{pods_installed} total #{'pod'.pluralize(pods_installed)} installed.".green, 
-              title_options)
+                        "There #{podfile_dependencies == 1 ? 'is' : 'are'} #{podfile_dependencies} " \
+                        "#{'dependency'.pluralize(podfile_dependencies)} from the Podfile " \
+                        "and #{pods_installed} total #{'pod'.pluralize(pods_installed)} installed.".green,
+                        title_options)
     end
 
     # Runs the registered callbacks for the plugins post install hooks.
