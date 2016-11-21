6568fce382bf25d248552819d72dea88a1f73eb4
Ben Asher <benasher44@gmail.com>
2016-11-21 12:54:33 -0800

    Fixed rubocop issues

--- spec/unit/installer/analyzer_spec.rb
+++ spec/unit/installer/analyzer_spec.rb
@@ -765,10 +765,10 @@ module Pod
           analyzer = Pod::Installer::Analyzer.new(config.sandbox, podfile)
           result = analyzer.analyze
 
-          result.targets.select { |at| at.name == 'Pods-SampleProject' }.flat_map(&:pod_targets).map { |pt| "#{pt.name}" }.sort.uniq.should == [
-            'JSONKit',
-            'monkey',
-          ].sort
+          result.targets.select { |at| at.name == 'Pods-SampleProject' }.flat_map(&:pod_targets).map(&:name).sort.uniq.should == %w(
+            JSONKit
+            monkey
+          ).sort
         end
 
         it "raises when unable to find an extension's host target" do
