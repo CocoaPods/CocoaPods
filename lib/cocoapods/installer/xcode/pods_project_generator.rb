ae57dcc18294f8c733b6ccdc1204b90cebe77b3a
Renzo Crisóstomo <renzo.crisostomomedina@here.com>
2017-08-18 13:50:36 +0200

    Remove bogus PROVISIONING_PROFILE_SPECIFIER value from Pods project
    This value breaks code signing on Xcode 9 using CODE_SIGN_IDENTITY environment variable

--- lib/cocoapods/installer/xcode/pods_project_generator.rb
+++ lib/cocoapods/installer/xcode/pods_project_generator.rb
@@ -153,7 +153,6 @@ module Pod
               build_configuration.build_settings['STRIP_INSTALLED_PRODUCT'] = 'NO'
               build_configuration.build_settings['CLANG_ENABLE_OBJC_ARC'] = 'YES'
               build_configuration.build_settings['CODE_SIGNING_REQUIRED'] = 'NO'
-              build_configuration.build_settings['PROVISIONING_PROFILE_SPECIFIER'] = 'NO_SIGNING/' # a bogus provisioning profile ID assumed to be invalid
             end
           end
         end
