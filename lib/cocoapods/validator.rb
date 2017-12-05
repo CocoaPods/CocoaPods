375b286a30b143691eed04484461a38ce3e18a99
Felix Krause <KrauseFx@users.noreply.github.com>
2017-12-04 18:48:53 -0800

    Update warning message

--- lib/cocoapods/validator.rb
+++ lib/cocoapods/validator.rb
@@ -402,7 +402,7 @@ module Pod
       return if url.downcase.start_with?('https://')
       warning('http', "The URL (#{url}) doesn't use the encrypted HTTPs protocol. " \
               'It is crucial for Pods to be transferred over a secure protocol to protect your users from man-in-the-middle attacks. '\
-              'Please update the URL to use https and try again.')
+              'This will be an error in future releases. Please update the URL to use https.')
     end
 
     # Performs validation for which version of Swift is used during validation.
