426aa00031605afa62a8c6f171cd49ff2492bea1
Paul Beusterien <paulbeusterien@google.com>
2018-02-02 13:12:50 -0800

    Updates for Xcodeproj 1.5.5 and CocoaPods #7393 (#137)

--- install_custom_module_name/after/Pods/Target Support Files/Pods-Example/Pods-Example-frameworks.sh
+++ install_custom_module_name/after/Pods/Target Support Files/Pods-Example/Pods-Example-frameworks.sh
@@ -4,7 +4,7 @@ set -u
 set -o pipefail
 
 if [ -z ${FRAMEWORKS_FOLDER_PATH+x} ]; then
-    # If FRAMEWORKS_FOLDER_PATH is not set, then there's no where for us to copy
+    # If FRAMEWORKS_FOLDER_PATH is not set, then there's nowhere for us to copy
     # frameworks to, so exit 0 (signalling the script phase was successful).
     exit 0
 fi
