374fa66996371ea5371ff9e4f2e18df117632750
Paul Beusterien <paulbeusterien@google.com>
2018-02-05 15:46:19 -0800

    target.rb change

--- lib/cocoapods/target.rb
+++ lib/cocoapods/target.rb
@@ -158,7 +158,7 @@ module Pod
     #
     def module_map_path
       basename = "#{label}.modulemap"
-      if requires_frameworks? || !respond_to?(:build_headers)
+      if !static_framework? && (requires_frameworks? || !respond_to?(:build_headers))
         support_files_dir + basename
       else
         build_headers.root + product_module_name + basename
