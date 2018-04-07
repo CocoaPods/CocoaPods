7212de81b40c1e976b97578736194bf37509778e
Paul Beusterien <paulbeusterien@google.com>
2018-04-07 10:47:59 -0700

    Fix modular header access to header_dirs

--- lib/cocoapods/target/pod_target.rb
+++ lib/cocoapods/target/pod_target.rb
@@ -600,7 +600,11 @@ module Pod
       dependent_targets = recursive_dependent_targets
       dependent_targets += recursive_test_dependent_targets if include_test_dependent_targets
       dependent_targets.each do |dependent_target|
-        header_search_paths.concat(sandbox.public_headers.search_paths(platform, dependent_target.pod_name, defines_module? && dependent_target.uses_modular_headers?(false)))
+        header_search_paths.concat(sandbox.public_headers.search_paths(platform,
+                                                                       dependent_target.pod_name,
+                                                                       defines_module? &&
+                                                                         dependent_target.uses_modular_headers?(false) &&
+                                                                         dependent_target.spec_consumers.none?(&:header_dir)))
       end
       header_search_paths.uniq
     end
