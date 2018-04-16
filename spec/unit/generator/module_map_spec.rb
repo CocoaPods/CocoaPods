08090c8507d5c76dee106d200b4a46b7222abdf2
Samuel Giddins <segiddins@segiddins.me>
2018-04-16 15:53:23 -0700

    Merge pull request #7638 from dnkoutso/system_module_maps
    Remove [system] declaration attribute from generated module maps
    
    (cherry picked from commit 0a93634a872c5c14592f764384694023f69381b7)
    
    # Conflicts:
    #	spec/cocoapods-integration-specs

--- spec/unit/generator/module_map_spec.rb
+++ spec/unit/generator/module_map_spec.rb
@@ -27,7 +27,7 @@ module Pod
       @pod_target.stubs(:requires_frameworks?).returns(false)
       @gen.save_as(path)
       path.read.should == <<-EOS.strip_heredoc
-        module BananaLib [system] {
+        module BananaLib {
           umbrella header "BananaLib-umbrella.h"
 
           export *
