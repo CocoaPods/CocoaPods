b55061395a9cef5cd173e7697ce402fa81411c17
Dimitris Koutsogiorgas <dnkoutso@gmail.com>
2018-08-13 17:19:56 -0700

    Correctly scope Swift language for each test spec

--- lib/cocoapods/installer/xcode/pods_project_generator/pod_target_installer.rb
+++ lib/cocoapods/installer/xcode/pods_project_generator/pod_target_installer.rb
@@ -268,7 +268,7 @@ module Pod
               product_type = target.product_type_for_test_type(test_type)
               name = target.test_target_label(test_spec)
               platform_name = target.platform.name
-              language = target.uses_swift_for_test_type?(test_type) ? :swift : :objc
+              language = target.uses_swift_for_test_spec?(test_spec) ? :swift : :objc
               test_native_target = project.new_target(product_type, name, platform_name, deployment_target, nil, language)
               test_native_target.product_reference.name = name
 
