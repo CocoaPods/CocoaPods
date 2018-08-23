b142febeb533af8df68c1be3f026efc96805400b
Dimitris Koutsogiorgas <dnkoutso@gmail.com>
2018-08-22 19:25:08 -0700

    Fix indentation in script phases

--- lib/cocoapods/generator/embed_frameworks_script.rb
+++ lib/cocoapods/generator/embed_frameworks_script.rb
@@ -52,9 +52,9 @@ module Pod
           trap 'on_error $LINENO' ERR
 
           if [ -z ${FRAMEWORKS_FOLDER_PATH+x} ]; then
-              # If FRAMEWORKS_FOLDER_PATH is not set, then there's nowhere for us to copy
-              # frameworks to, so exit 0 (signalling the script phase was successful).
-              exit 0
+            # If FRAMEWORKS_FOLDER_PATH is not set, then there's nowhere for us to copy
+            # frameworks to, so exit 0 (signalling the script phase was successful).
+            exit 0
           fi
 
           echo "mkdir -p ${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
@@ -84,8 +84,8 @@ module Pod
             local destination="${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
 
             if [ -L "${source}" ]; then
-                echo "Symlinked..."
-                source="$(readlink "${source}")"
+              echo "Symlinked..."
+              source="$(readlink "${source}")"
             fi
 
             # Use filter instead of exclude so missing patterns don't throw errors.
