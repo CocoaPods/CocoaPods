c79563cc331e727ed614e0bfc4240b0b365af1bc
Ben McIlwain <mcilwain@google.com>
2018-12-13 16:44:41 -0500

    Fix HTTPs -> HTTPS in warning message
    The entire word is an acronym, so "HTTPS" is correct.  "HTTPs" would be plural HTTP, which isn't the intended use here.

--- lib/cocoapods/validator.rb
+++ lib/cocoapods/validator.rb
@@ -467,7 +467,7 @@ module Pod
       return if spec.source.nil? || spec.source[:http].nil?
       url = URI(spec.source[:http])
       return if url.scheme == 'https' || url.scheme == 'file'
-      warning('http', "The URL (`#{url}`) doesn't use the encrypted HTTPs protocol. " \
+      warning('http', "The URL (`#{url}`) doesn't use the encrypted HTTPS protocol. " \
               'It is crucial for Pods to be transferred over a secure protocol to protect your users from man-in-the-middle attacks. '\
               'This will be an error in future releases. Please update the URL to use https.')
     end
