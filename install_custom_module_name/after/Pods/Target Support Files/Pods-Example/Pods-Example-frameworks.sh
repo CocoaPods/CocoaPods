6233e61d33084d16d2eaa11c62452c02bf2da354
Dimitris Koutsogiorgas <dimitris@squareup.com>
2019-05-21 11:18:36 -0400

    Update integration specs

--- install_custom_module_name/after/Pods/Target Support Files/Pods-Example/Pods-Example-frameworks.sh
+++ install_custom_module_name/after/Pods/Target Support Files/Pods-Example/Pods-Example-frameworks.sh
@@ -94,7 +94,7 @@ install_dsym() {
     binary="${DERIVED_FILES_DIR}/${basename}.framework.dSYM/Contents/Resources/DWARF/${basename}"
 
     # Strip invalid architectures so "fat" simulator / device frameworks work on device
-    if [[ "$(file "$binary")" == *"Mach-O dSYM companion"* ]]; then
+    if [[ "$(file "$binary")" == *"Mach-O "*"dSYM companion"* ]]; then
       strip_invalid_archs "$binary"
     fi
 
