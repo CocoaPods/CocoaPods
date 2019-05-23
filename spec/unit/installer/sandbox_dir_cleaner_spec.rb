4ff4d169c552676a1523db6aed862c59750b0f8b
Eric Amorde <eric.amorde@gmail.com>
2019-05-22 23:39:57 -0700

    Fix a regression in static library header dir paths
    Closes #8836

--- spec/unit/installer/sandbox_dir_cleaner_spec.rb
+++ spec/unit/installer/sandbox_dir_cleaner_spec.rb
@@ -4,13 +4,17 @@ module Pod
   describe Installer::SandboxDirCleaner do
     before do
       @pod_target = fixture_pod_target('banana-lib/BananaLib.podspec')
+      spec = fixture_spec('coconut-lib/CoconutLib.podspec')
+      spec.module_name = 'CoconutLibModule'
+      @other_pod_target = fixture_pod_target_with_specs([spec])
       @aggregate_target = AggregateTarget.new(config.sandbox, false, {}, [], Platform.ios,
                                               fixture_target_definition('MyApp'), config.sandbox.root.dirname, nil,
                                               nil, {})
-      @cleaner = Installer::SandboxDirCleaner.new(config.sandbox, [@pod_target], [@aggregate_target])
+      @cleaner = Installer::SandboxDirCleaner.new(config.sandbox, [@pod_target, @other_pod_target], [@aggregate_target])
       @project = Project.new(config.sandbox.project_path)
       @sandbox = config.sandbox
       FileUtils.mkdir_p(@sandbox.target_support_files_dir(@pod_target.name))
+      FileUtils.mkdir_p(@sandbox.target_support_files_dir(@other_pod_target.name))
       FileUtils.mkdir_p(@sandbox.target_support_files_dir(@aggregate_target.name))
     end
 
@@ -32,15 +36,25 @@ module Pod
       random_pod_public_headers = @sandbox.public_headers.root.join('PublicPod')
       banana_lib_public_headers = @sandbox.public_headers.root.join('BananaLib')
       banana_lib_private_headers = @pod_target.build_headers.root.join('BananaLib')
+      coconut_lib_public_headers = @sandbox.public_headers.root.join('CoconutLibModule')
+      coconut_lib_private_headers = @pod_target.build_headers.root.join('CoconutLibModule')
       FileUtils.mkdir_p(random_pod_private_headers)
       FileUtils.mkdir_p(random_pod_public_headers)
       FileUtils.mkdir_p(banana_lib_public_headers)
       FileUtils.mkdir_p(banana_lib_private_headers)
+      FileUtils.mkdir_p(coconut_lib_public_headers)
+      FileUtils.mkdir_p(coconut_lib_private_headers)
       @cleaner.expects(:remove_dir).with(random_pod_private_headers)
       @cleaner.expects(:remove_dir).with(random_pod_public_headers)
+      @cleaner.expects(:remove_dir).with(coconut_lib_public_headers).never
+      @cleaner.expects(:remove_dir).with(coconut_lib_private_headers)
       @cleaner.clean!
-      banana_lib_public_headers.should.exist?
-      banana_lib_private_headers.should.exist?
+      FileUtils.rm_rf(random_pod_private_headers)
+      FileUtils.rm_rf(random_pod_public_headers)
+      FileUtils.rm_rf(banana_lib_public_headers)
+      FileUtils.rm_rf(banana_lib_private_headers)
+      FileUtils.rm_rf(coconut_lib_public_headers)
+      FileUtils.rm_rf(coconut_lib_private_headers)
     end
   end
 end
