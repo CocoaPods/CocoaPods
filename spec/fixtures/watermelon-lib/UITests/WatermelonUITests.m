56631e6f766e7664d23c9da0a810cb29ba38be20
Yavuz Nuzumlali <manuyavuz@gmail.com>
2019-07-16 21:33:41 -0400

    [fixtures] Add test file to Watermelon UI test spec

---
+++ spec/fixtures/watermelon-lib/UITests/WatermelonUITests.m
@@ -0,0 +1,24 @@
+#import <KIF/KIF.h>
+
+@interface WatermelonUITests : KIFTestCase
+
+@end
+
+@implementation WatermelonUITests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
