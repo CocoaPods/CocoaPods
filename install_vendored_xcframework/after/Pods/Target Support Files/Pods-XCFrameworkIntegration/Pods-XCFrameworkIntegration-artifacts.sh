366c3fdfc168b5cc10ee39151781c5dc4b45f589
Eric Amorde <eric.amorde@gmail.com>
2020-02-29 15:42:35 -0800

    Fix for XCFrameworks on macOS

--- install_vendored_xcframework/after/Pods/Target Support Files/Pods-XCFrameworkIntegration/Pods-XCFrameworkIntegration-artifacts.sh
+++ install_vendored_xcframework/after/Pods/Target Support Files/Pods-XCFrameworkIntegration/Pods-XCFrameworkIntegration-artifacts.sh
@@ -87,7 +87,7 @@ install_xcframework() {
   if [[ "$PLATFORM_NAME" == *"simulator" ]]; then
     target_variant="simulator"
   fi
-  if [[ "$EFFECTIVE_PLATFORM_NAME" == *"maccatalyst" ]]; then
+  if [[ ! -z ${EFFECTIVE_PLATFORM_NAME+x} && "$EFFECTIVE_PLATFORM_NAME" == *"maccatalyst" ]]; then
     target_variant="maccatalyst"
   fi
   for i in ${!paths[@]}; do
