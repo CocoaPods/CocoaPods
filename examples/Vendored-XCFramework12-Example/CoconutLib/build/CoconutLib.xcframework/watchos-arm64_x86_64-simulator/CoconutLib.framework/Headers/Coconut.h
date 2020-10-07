550e4a47883557dc1d4e8398bf4d0b84527b71fd
John McIntosh <johntmcintosh@gmail.com>
2020-10-07 15:02:40 -0500

    Build the xcframework and install the pod in the example project

---
+++ examples/Vendored-XCFramework12-Example/CoconutLib/build/CoconutLib.xcframework/watchos-arm64_x86_64-simulator/CoconutLib.framework/Headers/Coconut.h
@@ -0,0 +1,18 @@
+//
+//  Coconut.h
+//  CoconutLib
+//
+//  Created by John McIntosh on 10/7/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Coconut : NSObject
+
+- (void)makeCoconuts;
+
+@end
+
+NS_ASSUME_NONNULL_END
