1520d64a4842fdef1afa316bcd6d85b8146fb53f
Dimitris Koutsogiorgas <dimitris@squareup.com>
2021-07-30 20:53:14 +0300

    Update integration specs

--- install_vendored_static_library_xcframework/after/Pods/Target Support Files/BananaLib/BananaLib-xcframeworks.sh
+++ install_vendored_static_library_xcframework/after/Pods/Target Support Files/BananaLib/BananaLib-xcframeworks.sh
@@ -149,5 +149,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/../BananaLib/CoconutLib.xcframework" "CoconutLib" "library" "ios-arm64" "ios-x86_64-maccatalyst" "ios-arm64_x86_64-simulator"
+install_xcframework "${PODS_ROOT}/../BananaLib/CoconutLib.xcframework" "BananaLib" "library" "ios-arm64" "ios-x86_64-maccatalyst" "ios-arm64_x86_64-simulator"
 
