1cc999752c9de7d217b13b23cb2515280257fca0
Dimitris Koutsogiorgas <dimitris@squareup.com>
2021-08-13 00:42:36 +0300

    Update integration specs

--- install_vendored_static_xcframework/after/Pods/Target Support Files/BananaLib/BananaLib-xcframeworks.sh
+++ install_vendored_static_xcframework/after/Pods/Target Support Files/BananaLib/BananaLib-xcframeworks.sh
@@ -111,7 +111,7 @@ install_xcframework() {
   local basepath="$1"
   local name="$2"
   local package_type="$3"
-  local paths=("${@:3}")
+  local paths=("${@:4}")
 
   # Locate the correct slice of the .xcframework for the current architectures
   select_slice "${paths[@]}"
